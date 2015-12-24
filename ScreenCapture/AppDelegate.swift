05ced2c4b859bd1916640b8a641f68002c032dbb
Petri Damstén <petri.damsten@kdemail.net>
2015-12-24 06:29:09 +0200

    Initial Commit

---
+++ ScreenCapture/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ScreenCapture
+//
+//  Created by Petri Damstén on 24.12.2015.
+//  Copyright © 2015 Petri Damstén. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
